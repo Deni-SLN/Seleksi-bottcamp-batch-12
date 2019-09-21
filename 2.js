ddc175180035125e197b1a5ded610c6608db51d8
Deni-SLN <54982866+Deni-SLN@users.noreply.github.com>
2019-09-21 14:32:10 +0700

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,17 @@
+<form action="somefile.json">
+        <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            pattern="[a-z]{5,9}"
+            id="username">
+    </form>
+    <br>
+            <form action="somefile.json">
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        pattern="[a-z,A-Z,1-9]{10}"
+                        id="password">
+                </form>
